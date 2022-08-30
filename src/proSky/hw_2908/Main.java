5dab9d1d954b52d1c0d26c628aff52339faa0291
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-08-30 16:58:51 +0300

    Simple level

--- src/proSky/hw_2908/Main.java
+++ src/proSky/hw_2908/Main.java
@@ -1,5 +1,15 @@
+package proSky.hw_2908;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author lev = new Author("Lev", "Tolstoy");
+        Author max = new Author("Maxim", "Gorkiy");
+        Book warAndPeace = new Book("War and Peace", lev, 1934);
+        Book mother = new Book("Mother", max, 1920);
+        mother.setYearPublication(1929);
+
+        System.out.println("Название книги - " + warAndPeace.getName() + ", Автор - " + warAndPeace.getAuthor().getFirstName() + " " +warAndPeace.getAuthor().getLastName() + ", Год публикации " + warAndPeace.getYearPublication());
+        System.out.println("Название книги - " + mother.getName() + ", Автор - " + mother.getAuthor().getFirstName() + " " +mother.getAuthor().getLastName() + ", Год публикации " + mother.getYearPublication());
+
     }
 }
